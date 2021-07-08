0448bb941f48ff62af05054b34c7514091bb9f09
gotaek <64013443+gotaek@users.noreply.github.com>
2021-07-08 11:24:41 +0900

    merge api

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,27 +1,52 @@
 import React, { useState, useEffect } from 'react';
 import qs from 'qs';
-import searchFetch from '../API/searchFetch';
+import axios from 'axios';
 
+const tokenStr = '88b41cd7f2b33cf873ba436febf7b0ea';
 const SearchPage = ({ location }: any) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const query: any = qs.parse(location.search, {
     ignoreQueryPrefix: true,
   });
+
   const search_word: string = query.q;
 
   useEffect(() => {
-    setLoading(true);
-    searchFetch(search_word).then((res: any) => {
-      setData(res.data.documents);
-      console.log(data);
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          'https://dapi.kakao.com/v3/search/book?target=title',
+          {
+            params: { query: `${search_word}` },
+            headers: {
+              Authorization: `KakaoAK ${tokenStr}`,
+            },
+          },
+        );
+        setData(response.data.documents);
+        console.log(data);
+      } catch (e) {
+        console.log(e);
+      }
       setLoading(false);
-    });
+    };
+    fetchData();
   }, []);
+  if (loading) {
+    return <div> loading </div>;
+  }
   if (!data) {
     return null;
   }
-  return <h2>{data && <div>search success!</div>}</h2>;
+
+  return (
+    <h2>
+      {data && console.log(data)}
+      <div>itme</div>
+    </h2>
+  );
 };
 
 export default SearchPage;
