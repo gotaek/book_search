81de6cc914618765756a39983c7455e0b5345122
gotaek <64013443+gotaek@users.noreply.github.com>
2021-07-18 00:00:46 +0900

    infinite scroll

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -5,6 +5,7 @@ import { axiosGetData } from '../API/axiosGetData';
 import NonePage from './NonePage';
 import { RouteComponentProps } from 'react-router-dom';
 
+/*Interface*/
 export interface IState {
   data: {
     authors: string[];
@@ -21,21 +22,25 @@ export interface IState {
     url: string;
   };
   key: string;
-  ref: any;
 }
+
+/* SearchPage component */
 const SearchPage: React.FC<RouteComponentProps> = ({
   location,
 }: RouteComponentProps) => {
-  const [data, setData] = useState<IState['data'][]>([]);
-  const [loading, setLoading] = useState(false);
-  const [page, setPage] = useState(2);
-  const [target, setTarget] = useState(null);
+  const [data, setData] = useState<IState['data'][]>([]); //data를 관리하기 위함
+  const [loading, setLoading] = useState(false); //loading 상태를 관리
+  const [page, setPage] = useState(2); //page를 관리하여 api 요청을 하기 위함
+  const [target, setTarget] = useState(null); //infinite scroll을 위한 ref
+  const [pageEnd, setPageEnd] = useState(false); //마지막 페이지인지 아닌지를 확인
 
+  /*쿼리 파싱*/
   const query: any = qs.parse(location.search, {
     ignoreQueryPrefix: true,
   });
   const search_word: string = query.q;
 
+  /* 다른 쿼리 스트링이 들어오면 새롭게 데이터를 fetch */
   useEffect(() => {
     window.scrollTo(0, 0);
     setData([]);
@@ -46,6 +51,9 @@ const SearchPage: React.FC<RouteComponentProps> = ({
         const response = axiosGetData(search_word);
         response.then((res) => {
           setData(res.data.documents);
+          if (res.data.meta.is_end === true) {
+            setPageEnd(true);
+          }
         });
       } catch (e) {
         console.log(e);
@@ -55,25 +63,34 @@ const SearchPage: React.FC<RouteComponentProps> = ({
     fetchData();
   }, [search_word]);
 
+  /* 스크롤 시 새롭게 데이터를 가져오기 위한 함수 */
   const fetchMoreData = () => {
     setPage(page + 1);
     const response = axiosGetData(search_word, page);
     response.then((res) => {
       if (res.data.meta.is_end === false) {
         const newData = [...data, ...res.data.documents];
-        console.log(newData);
         setData(newData);
+      } else {
+        setPageEnd(true);
       }
     });
   };
 
+  /* infinite scroll */
   const onIntersect: IntersectionObserverCallback = async (
     [entry],
     observer: IntersectionObserver,
   ) => {
     if (entry.isIntersecting) {
+      setLoading(true);
       observer.unobserve(entry.target);
-      await fetchMoreData();
+      if (pageEnd === false) {
+        await fetchMoreData();
+      } else {
+        observer.disconnect();
+      }
+      setLoading(false);
       observer.observe(entry.target);
     }
   };
@@ -82,14 +99,14 @@ const SearchPage: React.FC<RouteComponentProps> = ({
     let observer: IntersectionObserver;
     if (target) {
       observer = new IntersectionObserver(onIntersect, {
-        threshold: 0.5,
-        root: document.querySelector('body'),
+        threshold: 1,
       });
       observer.observe(target);
     }
     return () => observer && observer.disconnect();
   }, [target]);
 
+  /* loading과 null data 처리 */
   if (loading) {
     return <div> loading </div>;
   }
@@ -105,24 +122,14 @@ const SearchPage: React.FC<RouteComponentProps> = ({
         <div>
           <ul className="grid">
             {data.map((d, index) => {
-              const lastEl = index === data.length - 1;
-              console.log(lastEl);
               return parseInt(d.isbn) % 2 === 0 ? (
-                <BookListItem
-                  key={d.isbn + index}
-                  data={d}
-                  ref={lastEl ? setTarget : null}
-                />
+                <BookListItem key={d.isbn + index} data={d} />
               ) : (
-                <BookListItem
-                  key={d.isbn + index}
-                  data={d}
-                  ref={lastEl ? setTarget : null}
-                />
+                <BookListItem key={d.isbn + index} data={d} />
               );
             })}
           </ul>
-          <button onClick={fetchMoreData}>더 보기</button>
+          <div ref={setTarget} />
         </div>
       )}
     </>
